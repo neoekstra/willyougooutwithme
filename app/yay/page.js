f30164d4bc5291e1651a6879ce5a5dcde66d2649
neoekstra <157246097+neoekstra@users.noreply.github.com>
2024-02-08 22:15:43 +0400

    Update page.js

--- app/yay/page.js
+++ app/yay/page.js
@@ -3,7 +3,7 @@ export default function page() {
   return (
     <div className='box'>
       <img alt='bugcat yayyy' className='image' src='/bugcat-yay.gif' />
-      Supersennn♡
+      herseyden daha deyerlisen♡
     </div>
   )
 
