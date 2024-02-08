b98f6aba70cf04ce234b3f04ad6fb5ff0f4558bb
neoekstra <157246097+neoekstra@users.noreply.github.com>
2024-02-08 22:17:13 +0400

    Update page.js

--- app/page.js
+++ app/page.js
@@ -23,7 +23,7 @@ export default function page() {
     <div className='box'>
       <img alt={please ? 'capoo excited' : 'capoo flowers'} className='gif' 
         src={please ? '/capoo-excited.gif' : '/capoo-flowers.gif'} />
-      Size getirim kitabi?
+      Barisdiq bebiss?
       <div className='btns'>
         <Link className='yes' href='/yay'>
           <button onMouseOver={prettyPlease} onMouseOut={prettyPlease}>
